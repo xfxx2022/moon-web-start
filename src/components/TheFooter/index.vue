2963753fb0ccc599a4d71c8ae6e5305f6cdeb3fc
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2025-01-18 22:04:45 +0800

    Update index.vue

--- src/components/TheFooter/index.vue
+++ src/components/TheFooter/index.vue
@@ -6,6 +6,6 @@ defineOptions({
 
 <template>
   <div my-24 flex-center gap-x-8>
-    <a class="nav-link" href="https://blog.1314151.xyz" target="_blank">
+    <a i-carbon:logo-github icon-btn href="https://blog.1314151.xyz" target="_blank" />
   </div>
 </template>
